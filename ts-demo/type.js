0f4e1f3200717aa851ed8d80116bc72b55769ad0
ByeolNabi <20183338@office.deu.ac.kr>
2024-05-03 12:54:29 +0900

    ts demo

---
+++ ts-demo/type.js
@@ -0,0 +1,10 @@
+"use strict";
+let student = {
+    name: "john",
+    course: "typescript",
+    score: 100,
+    grade: function () {
+        console.log("a");
+    },
+};
+student.name = "lee";
---
+++ ts-demo/type.ts
@@ -0,0 +1,10 @@
+let student = {
+  name: "john",
+  course: "typescript",
+  score: 100,
+  grade: function () {
+    console.log("a");
+  },
+};
+
+student.name = "lee";
