95177285f150b249c0f35658cfc5a2bef2f64616
ByeolNabi <20183338@office.deu.ac.kr>
2024-05-05 13:54:56 +0900

    using enum in class

--- ts-demo/interface-demo.js
+++ ts-demo/interface-demo.js
@@ -1,12 +1,48 @@
 "use strict";
-function getInfo(id) {
-    return {
-        stdId: id,
-        stdName: "string",
-        age: 100,
-        gender: "female",
-        course: "javascript",
-        completed: true,
-    };
+// 열거형 (변수에 자동적으로 숫자가 저장된다.)
+var GenderType;
+(function (GenderType) {
+    GenderType[GenderType["Male"] = 324] = "Male";
+    GenderType[GenderType["Female"] = 325] = "Female";
+    GenderType["GenderNeutral"] = "Neutral";
+})(GenderType || (GenderType = {}));
+// 인터페이스는 클래스를 만들 때 설계도가 되어줄 수 있다.
+class MyStudent {
+    constructor() {
+        this.stdId = 990709;
+        this.stdName = "park";
+        this.age = 30;
+        this.gender = GenderType.Male;
+        this.course = "node.js";
+        this.completed = true;
+    }
+    setName(name) {
+        this.stdName = name;
+        console.log("이름 설정 : " + this.stdName);
+    }
 }
-console.log(getInfo(534));
+const myInstance = new MyStudent();
+myInstance.setName("엘리스");
+// function getInfo(id: number): Student {
+//   return {
+//     stdId: id,
+//     stdName: "string",
+//     age: 100,
+//     gender: "female",
+//     course: "javascript",
+//     completed: true,
+//   };
+// }
+// console.log(getInfo(534));
+// let std: Student = {
+//   stdId: 990709,
+//   stdName: "string",
+//   age: 100,
+//   gender: GenderType.Male,
+//   course: "javascript",
+//   completed: true,
+// };
+// function setInfo(student: Student): void {
+//   console.log(student);
+// }
+// setInfo(std);
--- ts-demo/interface-demo.ts
+++ ts-demo/interface-demo.ts
@@ -1,22 +1,62 @@
+// 열거형 (변수에 자동적으로 숫자가 저장된다.)
+enum GenderType {
+  Male = 324,
+  Female,
+  GenderNeutral = "Neutral",
+}
 // 인터페이스
 interface Student {
   stdId: number;
   stdName: string;
   age: number;
-  gender: string;
+  gender: GenderType;
   course: string;
   completed: boolean;
+  //setName(name: string): void;
+  setName: (name: string) => void;
+  //getName: () => string;
 }
 
-function getInfo(id: number): Student {
-  return {
-    stdId: id,
-    stdName: "string",
-    age: 100,
-    gender: "female",
-    course: "javascript",
-    completed: true,
-  };
+// 인터페이스는 클래스를 만들 때 설계도가 되어줄 수 있다.
+class MyStudent implements Student {
+  stdId = 990709;
+  stdName = "park";
+  age = 30;
+  gender = GenderType.Male;
+  course = "node.js";
+  completed = true;
+
+  setName(name: string): void {
+    this.stdName = name;
+    console.log("이름 설정 : " + this.stdName);
+  }
 }
 
-console.log(getInfo(534));
+const myInstance = new MyStudent();
+myInstance.setName("엘리스");
+
+// function getInfo(id: number): Student {
+//   return {
+//     stdId: id,
+//     stdName: "string",
+//     age: 100,
+//     gender: "female",
+//     course: "javascript",
+//     completed: true,
+//   };
+// }
+// console.log(getInfo(534));
+
+// let std: Student = {
+//   stdId: 990709,
+//   stdName: "string",
+//   age: 100,
+//   gender: GenderType.Male,
+//   course: "javascript",
+//   completed: true,
+// };
+
+// function setInfo(student: Student): void {
+//   console.log(student);
+// }
+// setInfo(std);
