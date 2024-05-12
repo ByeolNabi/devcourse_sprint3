3d94fbab2e2d3513273ed10d6deec6f9c9eb334e
ByeolNabi <20183338@office.deu.ac.kr>
2024-05-12 12:56:58 +0900

    inline style

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,35 +1,28 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("react");
-const react_svg_1 = __importDefault(require("./assets/react.svg"));
-const vite_svg_1 = __importDefault(require("/vite.svg"));
 require("./App.css");
 function App() {
-    const [count, setCount] = (0, react_1.useState)(0);
-    return (<>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={vite_svg_1.default} className="logo" alt="Vite logo"/>
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={react_svg_1.default} className="logo react" alt="React logo"/>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>);
+    let name = "리액트";
+    const style = {
+        backgroundColor: "black",
+        color: "white",
+        fontSize: "48px",
+        funtWeight: "bold",
+        padding: "20px",
+    };
+    return (<div style={style}>
+      <h1 className="test">
+        Hello {name === "리액트" ? <h1>YES</h1> : null}!!
+      </h1>
+      <p>반갑습니다.</p>
+    </div>);
 }
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hello, 리액트!!",
+//     React.createElement("p", null, "반갑습니다.")
+//   );
+// }
 exports.default = App;
--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let name = "리액트";
+  const style = {
+    backgroundColor: "black",
+    color: "white",
+    fontSize: "48px",
+    funtWeight: "bold",
+    padding: "20px",
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={style}>
+      <h1 className="test">
+        Hello {name === "리액트" ? <h1>YES</h1> : null}!!
+      </h1>
+      <p>반갑습니다.</p>
+    </div>
+  );
 }
 
-export default App
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hello, 리액트!!",
+//     React.createElement("p", null, "반갑습니다.")
+//   );
+// }
+
+export default App;
